09574cd7eed1bd37a7ae6eef2acfbfe2ddb6eb94
esthel <esthel9026@naver.com>
2023-10-21 22:32:40 +0900

    Feat: 도형추가, 반응형적용, 회전

--- README.md
+++ README.md
@@ -11,7 +11,8 @@
 카메라가 3D object(🟰scene)을 비추고, 뒤의 renderer에 그려내기
 ➡️ 카메라, 3D object, renderer 순으로 존재
 
-## [Notion](https://sudden-mat-e7c.notion.site/3js-ddec650cf22c406dbf1a6995c3a71a45)
+## [threejs 공식문서](https://threejs.org/)
+## [Notion](https://sudden-mat-e7c.notion.site/three-js-ddec650cf22c406dbf1a6995c3a71a45?pvs=4)
 
 <div align="center">
   <img src="" width="800" >
--- js/app.js
+++ js/app.js
@@ -11,19 +11,50 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-
-// 캔버스
-const canvas = document.querySelector('#ex-03');
+camera.position.z = 2; // 도형이 안나온다면 카메라 위치 조절해보기
 
 // 렌더러
-const renderer = new THREE.WebGLRenderer({ canvas }); // 빈 인자로 전달 시 canvas 만들어줌 ➡️ appendChild 필요
+const renderer = new THREE.WebGLRenderer({ antialias: true }); // 곡선 매끄럽게
 renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+// 도형
+const geometry1 = new THREE.BoxGeometry(0.5, 0.5, 0.5); // 정육면체, x, y, z
+const meterial1 = new THREE.MeshStandardMaterial({ color: 0x999999 });
+const obj1 = new THREE.Mesh(geometry1, meterial1);
+obj1.position.x = -1; // 좌측으로 1만큼 이동
+scene.add(obj1);
+
+const geometry2 = new THREE.ConeGeometry(0.4, 0.7, 6); // 각뿔, 반지름, 높이, n각뿔
+const meterial2 = new THREE.MeshStandardMaterial({ color: 0x999999 });
+const obj2 = new THREE.Mesh(geometry2, meterial2);
+scene.add(obj2);
+
+const geometry3 = new THREE.IcosahedronGeometry(0.4, 0); // 정이십면체, 반지름, 각 개수(크면 원에 가까워짐)
+const meterial3 = new THREE.MeshStandardMaterial({ color: 0x999999 });
+const obj3 = new THREE.Mesh(geometry3, meterial3);
+obj3.position.x = 1;
+scene.add(obj3);
 
 const render = time => {
   time *= 0.001; // 초 단위로 바꾸기
-  // cube.rotation.x = time;
-  // cube.rotation.y = time;
+  obj1.rotation.x = time; // 회전시키기
+  obj1.rotation.y = time;
+  obj2.rotation.x = time;
+  obj2.rotation.y = time;
+  obj3.rotation.x = time;
+  obj3.rotation.y = time;
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 };
 requestAnimationFrame(render);
+
+// 반응형 ➡️ 첫 렌더링 후 사이즈 변경 시 적용
+const onWindowResize = () => {
+  camera.aspect = window.innerWidth / window.innerHeight; // 좌우비율 바뀌어도 유지
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+};
+
+window.addEventListener('resize', onWindowResize);
