056f8da868c0bd83eb99d3d823d89d8ab4f2d34c
esthel <esthel9026@naver.com>
2023-10-31 22:32:47 +0900

    Feat: sphere args

--- r3f/src/Element3D.tsx
+++ r3f/src/Element3D.tsx
@@ -12,8 +12,10 @@ const MakeBox = (props: MeshProps) => {
 };
 
 const Element3D = () => {
-  const refMesh = useRef<Mesh>(null);
-  const refWireMesh = useRef<Mesh>(null);
+  const refMeshBox = useRef<Mesh>(null);
+  const refWireMeshBox = useRef<Mesh>(null);
+  const refMeshSphere = useRef<Mesh>(null);
+  const refWireMeshSphere = useRef<Mesh>(null);
 
   const { xSize, ySize, zSize, xSegments, ySegments, zSegments } = useControls({
     // 마우스로 값 조정할 수 있도록 UI 제공
@@ -26,18 +28,55 @@ const Element3D = () => {
     zSegments: { value: 1, min: 1, max: 10, step: 1 }
   });
 
+  const {
+    radius,
+    widthSegments,
+    heightSegments,
+    phiStart,
+    phiLength,
+    thetaStart,
+    thetaLength
+  } = useControls({
+    radius: { value: 1, min: 0.1, max: 5, step: 0.01 },
+    widthSegments: { value: 32, min: 3, max: 256, step: 1 },
+    // 수평분할 ➡️ 기본값 32, 최소 3(삼각형이 최소)
+    heightSegments: { value: 32, min: 2, max: 256, step: 1 },
+    // 수직분할
+    phiStart: { value: 0, min: 0, max: 360, step: 0.1 },
+    // 시작 위치 각도로 조정
+    phiLength: { value: 360, min: 0, max: 360, step: 0.1 },
+    // 원주 길이
+    thetaStart: { value: 0, min: 0, max: 180, step: 0.1 },
+    // 구의 수직 시작 위치 ➡️ 잘린 구의 시작 위치 조절
+    thetaLength: { value: 180, min: 0, max: 180, step: 0.1 }
+    // 구의 수직 길이 ➡️ 잘려서 표현
+  });
+
   useFrame((state, delta) => {
     // delta ➡️ 이전, 현재 시간 차이(ms 단위)
     // 매 프레임이 렌더링되기 직전 실행
-    if (refMesh.current) refMesh.current.rotation.y += delta; // y축으로 회전
-    if (refWireMesh.current) refWireMesh.current.rotation.y += delta;
+    if (refMeshBox.current) refMeshBox.current.rotation.y += delta; // y축으로 회전
+    if (refWireMeshBox.current) refWireMeshBox.current.rotation.y += delta;
   });
 
   useEffect(() => {
-    if (refMesh.current && refWireMesh.current)
-      refWireMesh.current.geometry = refMesh.current.geometry;
+    if (refMeshBox.current && refWireMeshBox.current)
+      refWireMeshBox.current.geometry = refMeshBox.current.geometry;
     // refWireMesh에 refMesh 아래의 <boxGeometry {...props} />와 똑같이 생김
   }, [xSize, ySize, zSize, xSegments, ySegments, zSegments]);
+
+  useEffect(() => {
+    if (refMeshSphere.current && refWireMeshSphere.current)
+      refWireMeshSphere.current.geometry = refMeshSphere.current.geometry;
+  }, [
+    radius,
+    widthSegments,
+    heightSegments,
+    phiStart,
+    phiLength,
+    thetaStart,
+    thetaLength
+  ]);
 
   return (
     <>
@@ -50,18 +89,41 @@ const Element3D = () => {
 
       {/* mesh 이용 육면체 생성 */}
       <mesh
-        ref={refMesh}
+        ref={refMeshBox}
         rotation={[0, THREE.MathUtils.degToRad(45), 0]} // y축으로 45도만큼 회전
       >
         <boxGeometry
           args={[xSize, ySize, zSize, xSegments, ySegments, zSegments]}
+          // 기본값은 다 1
         />
         <meshStandardMaterial color="#e67e22" opacity={0.5} transparent />
       </mesh>
 
-      <mesh ref={refWireMesh} rotation={[0, THREE.MathUtils.degToRad(45), 0]}>
+      <mesh
+        ref={refWireMeshBox}
+        rotation={[0, THREE.MathUtils.degToRad(45), 0]}
+      >
         <meshStandardMaterial emissive="#e67e22" wireframe />
         {/* emissive ➡️ 광원 영향 없이 자체 발광 */}
+      </mesh>
+
+      <mesh ref={refMeshSphere} position={[0, 0, 3]}>
+        <sphereGeometry
+          args={[
+            radius,
+            widthSegments,
+            heightSegments,
+            (phiStart * Math.PI) / 180, // 라디안 단위로 변경 필요
+            (phiLength * Math.PI) / 180,
+            (thetaStart * Math.PI) / 180,
+            (thetaLength * Math.PI) / 180
+          ]}
+        />
+        <meshStandardMaterial color="#d5c105" opacity={0.5} transparent />
+      </mesh>
+
+      <mesh ref={refWireMeshSphere} position={[0, 0, 3]}>
+        <meshStandardMaterial emissive="#d5c105" wireframe />
       </mesh>
 
       {/* drei 이용 육면체 생성 */}
